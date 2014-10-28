e6bff0f2bec1ec6b97fd7629cf5f6ebc505dc768
Abinoam Praxedes Marques Jr <abinoam@gmail.com>
2014-10-28 11:43:26 -0300

    Add RSpec spec

---
+++ spec/cbhpm_table_spec.rb
@@ -0,0 +1,63 @@
+require 'rspec'
+require 'cbhpm_table'
+
+describe CBHPMTable do
+  let(:cbhpm_table) do
+    CBHPMTable.new("spec/cbhpm/CBHPM_cut_for_testing.xlsx",
+                   CBHPMTable::HeaderFormat::CBHPM2012)
+  end
+
+  it { expect(cbhpm_table).to respond_to(:headers) }
+
+  describe "#headers_hash - the format hash for headers" do
+    it { expect(cbhpm_table.headers_hash).to eq CBHPMTable::HeaderFormat::CBHPM2012 }
+  end
+
+  describe "#headers - the first line of the CBHPM Table" do
+    it { expect(cbhpm_table.headers).to be_instance_of(Hash) }
+    it { expect(cbhpm_table.headers).to eq({ "code"=>"ID do Procedimento",
+                                        "name"=>"Descrição do Procedimento",
+                                        "cir_size"=>nil,
+                                        "uco"=>"Custo Operac.",
+                                        "aux_qty"=>"Nº de Aux.",
+                                        "an_size"=>"Porte Anestés."}) }
+  end
+
+  describe "#row - any row" do
+    let(:row) { cbhpm_table.row(2) }
+
+    it { expect(row).to be_instance_of(Hash) }
+    it { expect(row).to eq({"code"=>"10101012",
+                            "name"=>"Em consultório (no horário normal ou preestabelecido)",
+                            "cir_size"=>"2B",
+                            "uco"=>nil,
+                            "aux_qty"=>nil,
+                            "an_size"=>nil})}
+  end
+
+  describe "#each_row" do
+    describe "returns an Enumerator when no block given" do
+      it { expect(cbhpm_table.each_row).to be_instance_of Enumerator }
+    end
+
+    describe "iterates through each row" do
+      specify do
+        cbhpm_table.each_row do |row|
+          expect(row).to be_instance_of Hash
+          expect(row['code']).to match /\d{8}/
+        end
+      end
+    end
+  end
+
+  describe "#rows" do
+    let(:rows) { cbhpm_table.rows }
+
+    it { expect(rows).to be_instance_of Array }
+
+    describe ".first" do
+      it { expect(rows.first).to be_instance_of Hash }
+    end
+  end
+end
+
